6dc20eaf0ebaf0095a1b6535436661f47fd0443c
Meniskuri <giorgikapanadze9169@gmail.com>
2021-12-06 23:37:03 +0400

    martali giorgi

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,6 +18,25 @@ namespace Kapana
             Console.WriteLine("My name is " + saxeli);
             Console.WriteLine("Im " + asaki);
 
+            // დატა ტაიფს 
+            string names = "saxelebi"; // ტექსტების შესანახად 
+            char grade = 'A'; // ერთი სიმბოლოს შენახვა შემიძლია, ერთიანი ბრჭყალით  
+            int age = 30;
+            // float, double, decimal
+            double gpa = 3.9;
+            bool martalia = true; 
+            Boolean mtyuani = false;
+
+            Console.WriteLine(mtyuani);
+            Console.WriteLine("Im " + grade + gpa);
+
+            if (mtyuani == false)
+            {
+                Console.WriteLine(martalia);
+                Console.WriteLine("Giorgi");
+            }
+
+
             Console.ReadLine();
 
         }
